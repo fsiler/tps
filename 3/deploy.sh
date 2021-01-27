70c421abbbd97cfc626e106c09e9a2a5f4b65d83
Frank Siler <fsiler@gmail.com>
2021-01-27 06:10:17 -0600

    remove commented sections in deploy.sh

--- 3/deploy.sh
+++ 3/deploy.sh
@@ -1,6 +1,4 @@
 templateFile="template.json"
 paramFile="params.json"
-#az group create \
-#  --name myResourceGroupProd \
-#  --location "West US"
+
 az deployment group create --resource-group tpstest_group --template-file $templateFile --parameters $paramFile
